b3d7c9033d86baa5756ae15730789fda90e722bd
jonatas.88 <jonatas.88@gmail.com>
2022-08-04 00:33:21 +0900

    code sequence adjusts

--- database_functions.py
+++ database_functions.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 import sqlite3
 
-#function to connect to the database
+#CONNECTION TO DATABASE
 def connect_to_database(dbname):
     conn = sqlite3.connect(dbname)
     cur = conn.cursor()
     return conn, cur
 
-#create table if it doesn't exist
+#CREATE TABLE IF NOT EXISTS
 def create_table(conn, cur,table_name,columns):
     cur.execute("CREATE TABLE IF NOT EXISTS {} ({})".format(table_name,columns))
     conn.commit()
     print("Table {} created".format(table_name))
     return
 
-#close connection
+#CLOSE CONNECTION
 def close_connection(conn):
     conn.close()
     print("Connection closed")
     return
 
-#insert data into table
+#INSERT DATA INTO TABLE
 def insert_data(conn, cur, table_name,data):
     cur.execute("INSERT INTO {} VALUES ({})".format(table_name,data))
     conn.commit()
